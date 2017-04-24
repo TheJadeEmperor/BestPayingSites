ac9c3073de0b12257bada8cc48250b644db2092f
TheJadeEmperor <louie.social.media@gmail.com>
2017-04-24 12:45:52 -0400

    add http:// prefix to redirects

--- redirect.php
+++ redirect.php
@@ -1,5 +1,9 @@
 <?php 
 $url = $_GET['url']; 
+
+if (strpos($url,'http://') === false){
+    $url = 'http://'.$url;
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
