8048e06d58bafb9b8322b4ef8c423fcd7f714788
TheJadeEmperor <louie.online.acc@gmail.com>
2017-08-06 23:51:30 -0400

    PTC-MS - trafficantics

---
+++ site/trafficantics/index.php
@@ -0,0 +1,30 @@
+<?php
+$nusLink = 'http://neobuxultimatestrategy.com/';
+$msLink = 'http://neobuxultimatestrategy.com/minisite/';
+
+switch($_GET['page']) {
+	
+	case 'ptc-strategy':
+		$metaTitle = 'PTC Strategy'; 
+		$file = 'ptc-strategy.html';
+		break;
+	
+	case 'ptc-reviews':
+		$metaTitle = 'PTC Reviews'; 
+		$file = 'ptc-reviews.html';
+		break;
+	
+	case 'ptc-sites':
+		$metaTitle = 'PTC Sites'; 
+		$file = 'ptc-sites.html';
+		break;
+	case 'home':
+	default:
+		$metaTitle = 'PTC Sites'; 
+		$file = 'home.html';
+}
+
+include('header.html');
+include($file);
+include('footer.html');
+?>
