6f96f764ec9ca7bd31e96572a33b10211eb754b8
TheJadeEmperor <benjamin_louie@drugfree.org>
2019-02-24 18:00:26 -0500

    BPS Content

--- members/content/bps-guide.php
+++ members/content/bps-guide.php
@@ -1,9 +1,32 @@
+<?
+$MakeMoreMoneyWithSurveys = '/home2/codegeas/ebooks/bonus/MakeMoreMoneyWithSurveys.pdf';
+?>
 
-How to take surveys
+<h1>Guide to Getting Started with Surveys</h1>
 
-Make new gmail account 
+<p>The first step we recommend is setting up a brand new email account for survey companies. Sometimes they will send you a lot of offers via email, so it's a good idea to have a new account to keep things organized. </p>
+
+<p>You should join ALL our survey partners in our surveys database. This way you will receive surveys on a consistent basis. It can take 2-4 weeks after registering with a survey company for them to start sending you surveys, but don't worry if things start out slow. If you register with all our partners you will have more than enough tasks to do to fill up your time. 
+</p>
+
+<p>There are some software programs that will help you turbo-charge your surveys jobs. Download our free report by clicking the image below. </p>
 
 
-Use software
+<center>
+<form method=POST>
+	<table>
+		<tr>
+			<td width="200px" align="center">
+				<fieldset>
+					<button type="submit" name="dl" value="dl"><img src="<?=$dir?>images/members/pdf.png" />
+				</fieldset>
+				<br />	
+				<input type=submit name=dl value="Download MakeMoreMoneyWithSurveys.pdf" class="btn success"/>
+				<input type=hidden name=url value="<?=$MakeMoreMoneyWithSurveys ?>" />
+			</td>
+		</tr>
+	</table>
+</form>
+</center>
 
-Link to download PDF
+<p>The ebook will show you how to use Roboform, LastPass and Password Safe to help you make more money with surveys. </p>
