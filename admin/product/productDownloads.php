3c954454417bdc717426c5d34d5b78fe10eb88d4
TheJadeEmperor <louie.online.acc@gmail.com>
2019-07-10 16:29:59 -0400

    admin code

--- admin/product/productDownloads.php
+++ admin/product/productDownloads.php
@@ -2,13 +2,11 @@
 $adir = '../';
 include($adir.'adminCode.php');
 
-if($_GET['id']) 
-{
+if($_GET['id'])  {
     $pID = $_GET['id'];
     
-    if($_POST['add']) 
-    {
-        $ins = 'insert into downloads (
+    if($_POST['add']) {
+        $ins = 'INSERT INTO downloads (
         productID, 
         name,
         url
@@ -19,8 +17,7 @@ if($_GET['id'])
         
         mysql_query($ins, $conn) or die(mysql_error());
     }
-    else if($_POST['update'])
-    {
+    else if($_POST['update']) {
         $dbOptions = array(
         'tableName' => 'downloads',
         'dbFields' => array(
@@ -31,36 +28,39 @@ if($_GET['id'])
         
         dbUpdate($dbOptions);
     }
-    
-    if($_POST['del']) 
-    {
-        $del = 'delete from downloads where id="'.$_POST['id'].'"';
+	else if($_POST['delete_x'] && $_POST['delete_y']) {
+        $del = 'DELETE FROM downloads WHERE id="'.$_POST['downloadID'].'"';
         mysql_query($del, $conn) or die(mysql_error());
     }
+
+	
+	if($_GET['debug'] == 1) {
+		echo $ins; echo $del;
+		echo '<pre>'; print_r($_POST); echo '</pre>';
+	}
+	
     
     $properties = 'type="text" class="activeField"';
         
-    $sel = 'select *, d.id as downloadID from downloads d left join products p on p.id = d.productID
-    where productID="'.$pID.'" order by name asc';
+    $sel = 'SELECT *, d.id AS downloadID FROM downloads d LEFT JOIN products p ON p.id = d.productID WHERE productID="'.$pID.'" ORDER BY name ASC';
     $res = mysql_query($sel, $conn) or die(mysql_error());
     
     $c = 1;
-    while($d = mysql_fetch_assoc($res)) 
-    {   
-        $downloadsList .= '<form method=post>
-        <tr valign=top>
+    while($d = mysql_fetch_assoc($res)) {   
+        $downloadsList .= '<form method="POST">
+        <tr valign="top">
         <td>'.$c.'</td>
         <td>
-            <input '.$properties.' name="name" value="'.$d[name].'" size="25"/><br>
+            <input '.$properties.' name="name" value="'.$d['name'].'" size="25"/><br />
             <input type="submit" name="update" value="Update" />
         </td>
         <td>
-            <input '.$properties.' name="url" value="'.$d[url].'" size="60"/><br>
+            <input '.$properties.' name="url" value="'.$d['url'].'" size="60"/><br />
             <input type="submit" name="dl" value="Test Download">
         </td>
         <td>
-            <input type=hidden name="id" value="'.$d[downloadID].'" /> 
-            <input type=image name="del" value="del" src="'.$delImg.'" onclick="confirm(\'Are you sure?\');">
+            <input type="hidden" name="downloadID" value="'.$d['downloadID'].'" /> 
+            <input type="image" name="delete" value="" src="'.$delImg.'" onclick="confirm(\'Are you sure?\');">
         </td>
         </tr>
         </form>';
@@ -71,13 +71,17 @@ if($_GET['id'])
 }
 ?>
 <div class="moduleBlue"><h1>Downloads for <?=$itemName?></h1><div>
-    <a href="productNew.php?id=<?=$pID?>">Edit Product</a>
+	<center>   
+	<br />
+	<p><a href="productNew.php?id=<?=$pID?>"><button class="btn btn-success">Edit Product</button></a></p>
+	
+	</center>
 </div>
 </div>
 
 <p>&nbsp;</p>
 
-<table class="moduleBlue" cellspacing=0 cellpadding=2>    
+<table class="moduleBlue" cellspacing="0" cellpadding="2">    
     <tr>
         <th>#</th><th>Name</th><th>URL</th><th></th>
     </tr>
@@ -88,22 +92,25 @@ if($_GET['id'])
 
 <p>&nbsp;</p>
 
-<form method=post>
-<div class="moduleBlue"><h1>Add New Product</h1>
+<form method="POST">
+<div class="moduleBlue"><h1>Add New Download</h1>
 <div>
      <table>
          <tr>
              <td>Name of Download</td>
-             <td><input <?=$properties?> name=name value=""></td>
+             <td><input <?=$properties?> name="name" value="" /></td>
          </tr>
          <tr>
              <td>URL of Download</td>
-             <td><input <?=$properties?> name=url value=""></td>
+             <td><input <?=$properties?> name="url" value="" /></td>
          </tr>
          <tr>
-             <td><input type=submit name=add value="Add Download"/></td>
+             <td colspan="2" align="center"><input type="submit" name="add" value="Add Download" /></td>
          </tr>
      </table>   
 </div>
 </div>
 </form>
+
+<?
+include($adir.'adminFooter.php'); ?>
--- admin/product/productEmailsList.php
+++ admin/product/productEmailsList.php
@@ -3,24 +3,21 @@ $adir = '../';
 include($adir.'adminCode.php');
 
 //update download email options
-if($_POST[updateEmailOptions])
-{ 
-    $updEC = 'update settings set setting="'.$_POST['sendDownloadEmailCopy'].'" where opt="sendDownloadEmailCopy"';
+if($_POST['updateEmailOptions']) { 
+    $updEC = 'UPDATE settings SET setting="'.$_POST['sendDownloadEmailCopy'].'" WHERE opt="sendDownloadEmailCopy"';
     mysql_query($updEC, $conn) or die(mysql_error());
     
-    $updEA = 'update settings set setting="'.$_POST['sendDownloadEmailAddress'].'" where opt="sendDownloadEmailAddress"';
+    $updEA = 'UPDATE settings SET setting="'.$_POST['sendDownloadEmailAddress'].'" WHERE opt="sendDownloadEmailAddress"';
     mysql_query($updEA, $conn) or die(mysql_error());
     
     $val['sendDownloadEmailCopy'] = $_POST['sendDownloadEmailCopy'];
     $val['sendDownloadEmailAddress'] = $_POST['sendDownloadEmailAddress'];
 }
 
-if($val['sendDownloadEmailCopy'] == 'on')
-{
+if($val['sendDownloadEmailCopy'] == 'on') {
     $downloadEmailChecked = 'checked';
 } 
-else
-{
+else {
     $downloadEmailDisabled = 'disabled'; 
 }
 
@@ -32,45 +29,46 @@ $opt = array(
 
 $emailList = dbSelect($opt); 
 
-foreach($emailList as $e)
-{
+foreach($emailList as $e) {
     //get product info
-    $selP = 'select * from products where id="'.$e[productID].'"';
+    $selP = 'SELECT * FROM products WHERE id="'.$e['productID'].'"';
     $resP = mysql_query($selP, $conn) or die(mysql_error()); 
     $p = mysql_fetch_assoc($resP); 
+	
+	$pID = $e['productID'];
     
-    if($currentProduct == $p[itemName])
-    {
-        $theList .= '<tr>
+    if($currentProduct == $p['itemName']) {
+		
+        $theList .= '<tr title="'.$pID.'">
         <td></td>
-        <td><a href="productEmailsEdit.php?id='.$e[productID].'&type='.$e[type].'">'.$e[type].'</a></td>
-        <td>'.$e[subject].' </td>
+        <td><a href="productEmailsEdit.php?id='.$pID.'&type='.$e['type'].'">'.$e['type'].'</a></td>
+        <td>'.$e['subject'].' </td>
         </tr>';
     }
     else {
-        $itemName = $currentProduct = $p[itemName]; 
-        $theList .= '<tr><td></td></tr>
-        <tr>
-        <td><a href="productNew.php?id='.$e[productID].'">'.$itemName.'</a></td>
-        <td><a href="productEmailsEdit.php?id='.$e[productID].'&type='.$e[type].'">'.$e[type].'</a></td>
-        <td>'.$e[subject].' </td>
+        $itemName = $currentProduct = $p['itemName']; 
+        $theList .= '<tr><td colspan="3"></td></tr>
+        <tr title="'.$pID.'">
+        <td><a href="productNew.php?id='.$e['productID'].'">'.$itemName.'</a></td>
+        <td><a href="productEmailsEdit.php?id='.$e['productID'].'&type='.$e['type'].'">'.$e['type'].'</a></td>
+        <td>'.$e['subject'].' </td>
         </tr>';
     }
 }
 ?>
 
 <div class="moduleBlue"><h1>Download Emails</h1>
-<form method=post>
+<form method="POST">
     <table>
     <tr>
         <td>Send Myself A Copy of The Confirmation Email</td>
-        <td><input type=checkbox name="sendDownloadEmailCopy" <?=$downloadEmailChecked?> /></td>
+        <td><input type="checkbox" name="sendDownloadEmailCopy" <?=$downloadEmailChecked?> /></td>
     </tr>
     <tr>
-        <td>Send Email Copy to This Email Address<br /><input type=text name="sendDownloadEmailAddress" value="<?=$val['sendDownloadEmailAddress']?>" <?=$downloadEmailDisabled?> size="40" /></td>
+        <td>Send Email Copy to This Email Address<br /><input type="text" name="sendDownloadEmailAddress" value="<?=$val['sendDownloadEmailAddress']?>" <?=$downloadEmailDisabled?> size="40" /></td>
     </tr>
     <tr>
-        <td align="center"><input type="submit" name="updateEmailOptions" value="Update Email Options" /></td>
+        <td align="center"><input type="submit" name="updateEmailOptions" value="Update Email Options" class="btn btn-success" /></td>
     </tr>
     </table>
 </form>
@@ -78,7 +76,7 @@ foreach($emailList as $e)
 
 <p>&nbsp;</p>
 
-<table class="moduleBlue" cellspacing=0 cellpadding=5>
+<table class="moduleBlue" cellspacing="0" cellpadding="5">
 	<tr><th>Product</th><th>Type</th><th>Subject</th></tr>
 	<?=$theList?>
 </table>
