17f7c31e8b4e30efe16f10bd006297201dd9c357
TheJadeEmperor <louie.online.acc@gmail.com>
2019-08-27 11:43:06 -0400

    Update pp-links.php

--- members/content/pp-links.php
+++ members/content/pp-links.php
@@ -36,23 +36,42 @@ fieldset {
     
 	<legend>Advertising Sites</legend>
     <h3>Neobux</h3> 
-	<p> <a href="http://www.neobux.com/?r=KaibaCorp1" target="_BLANK">http://www.neobux.com</a></p>
-	<br />
+	<p><a href="http://www.neobux.com/?r=KaibaCorp1" target="_BLANK">http://www.neobux.com</a></p>
 	
+	<p>	To advertise on Neobux, go to your account and click on Advertisements. First, you have to select how many clicks you wish to buy, and the type of exposure. I recommend you start slow with $5 for 2500 clicks. Then, when you get some cash, you can advertise even more, and you earn more money every day just by using this method.</p> 	
+
+	<p>	When you are done setting up the advertisement, you need to purchase clicks. It will take a few hours for your advertisement to be approved. Wait and see the magic that happens the next time you open your Paypal account...it will blow your mind. </p>
+	<br />
+			
     <h3>ySense</h3>
 	<p> <a href="http://www.clixsense.com/?3373459" target="_BLANK">http://www.clixsense.com</a></p>
 	<br />
 	
+	<h3>Linkgrand</h3>
+	<p> <a href="http://linkgrand.com" target="_BLANK">http://linkgrand.com</a></p>
+	<br />
+	
     <h3>Adhitz</h3>
 	<p><a href="https://www.adhitz.com" target="_BLANK">http://www.adhitz.com</a></p>
+	
+	<p>Adhitz has a huge network of thousands of sites, which means there are many advertising options available to you. You can buy banner ads or just use plain text ads to sell the Paypal Booster.</p>
 	<br />
+	
+	<h3>Traffic Exchanges</h3>
+
+	<p>Traffic exchanges are a great way to get free traffic to your blog. There are many traffic exchanges out there, and many of them are bad, they will just waste your time. The Traffic Exchange List is a website that ranks all the traffic exchanges based on their quality of service. Go to that site to see what the top traffic exchanges are.</p>
+
+	<p>The sky is the limit for you and you can sell PayPal Booster Professional product anywhere and replicate what I have done in less than a few weeks. Remember, once your blog is ready and your ad is on Neobux and these websites, money will literally explode in your PayPal account. </p>
+
 	
     <h3>EasyHits4U</h3>
 	<p><a href="http://a.easyhits4u.com/splash8.php?ref=theemperor" target="_BLANK">http://www.easyhits4u.com</a></p>
-
+	<br />
     
-	<!--<h3>Top Traffic Exchanges</h3>
-	<a href="http://trexlist.com/manual-traffic-exchanges.shtml" target="_BLANK">http://trexlist.com/manual-traffic-exchanges.shtml</a>-->
+	<h3>Top Traffic Exchanges</h3>
+	
+	<p><a href="https://neobuxultimatestrategy/?p=top-traffic-exchanges-for-ptc-sites" target="_BLANK">https://neobuxultimatestrategy/?p=top-traffic-exchanges-for-ptc-sites</a>
+	</p>
 	
     <br /><br />
 </fieldset>
