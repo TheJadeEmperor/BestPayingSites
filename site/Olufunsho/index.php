33497af2b64ddad0860b7067dfe7ab45c630df07
TheJadeEmperor <benjamin_louie@drugfree.org>
2019-10-07 12:29:55 -0400

    ptc-ms

---
+++ site/Olufunsho/index.php
@@ -0,0 +1,46 @@
+<?php
+error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE); 
+
+function anchor($url, $extra, $displayText) {
+	return '<a href="'.$url.'" '.$extra.'>'.$displayText.'</a>'; 
+}
+
+function img($url, $extra, $altText) {
+	return '<img src="'.$url.'" '.$extra.' alt="'.$altText.'" title="'.$altText.'"/>'; 
+}
+
+$dir = '../../';
+$nusLink = 'http://neobuxultimatestrategy.com/?r='.$aff;
+$nusImg = 'http://neobuxultimatestrategy.com/images/banners/ebook1.jpg';
+$msLink = 'http://neobuxultimatestrategy.com/minisite/';
+$basicsLink = 'http://neobuxultimatestrategy.com/basics';
+$nusLink = 'http://neobuxultimatestrategy.com/?r='.$aff;
+$vcLink = 'http://neobuxultimatestrategy.com/video/?r='.$aff;
+
+switch($_GET['page']) {
+
+	case 'ptc-1':
+		$fileName = 'ptc-1.html';
+		break;
+	case 'ptc-2':
+		$fileName = 'ptc-2.html';
+		break;
+	case 'ptc-3':
+		$fileName = 'ptc-3.html';
+		break;
+	case 'ptc-4':
+		$fileName = 'ptc-4.html';
+		break;
+	case 'home':
+	default:
+		$meta = array(
+			'title' => "Olufunsho's PTC Website", 
+			'tags' => 'earn money ptc, make money ptc, earn with ptc, get paid to click',
+			'desc' => 'Here are some great ptc sites that you can make money from'); 
+		$fileName = 'home.html';
+}
+
+include('header.html');
+include($fileName);
+include('footer.html');
+?>
